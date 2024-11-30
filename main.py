cac43ba64e003b985b5adc6b61b026f2d3a52afe
douglas <douglasholanda3195@gmail.com>
2024-11-30 10:40:14 -0500

    Criação do primeiro endpoint de donations

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, Form, HTTPException, Query
+from pydantic import BaseModel
+import random
+import string
 
 
 app = FastAPI()
@@ -6,12 +9,23 @@ app = FastAPI()
 causes = []
 donations = [] 
 
-@app.post("/causes", status = 201)
-async def create_cause(cause : dict):
-    if any (sake["id"] == cause["id"] for sake in causes):
+class Cause(BaseModel) :
+    cause_id : int
+    cause_name: str
+    description : str
+    certification_code : str
+    amount : float
+
+class Donation(BaseModel) :
+    cause_id : int
+    value : float
+
+
+@app.post("/causes", status_code=201)
+async def create_cause(cause : Cause):
+    if any (sake["cause_id"] == cause["cause_id"] for sake in causes):
         raise HTTPException(status_code=400, detail="Cause already exists.")
 
-    cause["amount"] = 0.0
     causes.append(cause)
 
     return {
@@ -20,10 +34,10 @@ async def create_cause(cause : dict):
         "data" : cause
     }
 
-@app.get("/causes/{id}", status = 200)
+@app.get("/causes/{cause_id}", status_code = 200)
 async def get_cause_by_id(cause_id : int):
     for cause in causes:
-        if cause["id"] == id:
+        if cause.cause_id == cause_id:
             return {
                 "status" : "Success",
                 "message" : "Cause found successfully!",
@@ -31,22 +45,21 @@ async def get_cause_by_id(cause_id : int):
             }
         raise HTTPException(status_code=404, detail="Cause not found")
 
-@app.get("/causes", status=200)
-async def get_causes(cause : dict):
+@app.get("/causes", status_code=200)
+async def get_causes():
     if causes:
-        for cause in causes:
-            return {
-                "status" : "Success",
-                "message" : "Cause found successfully!",
-                "data" : cause
-            }
+        return {
+            "status" : "Success",
+            "message" : "Cause found successfully!",
+            "data" : causes
+        }
     raise HTTPException(status_code=401, detail="The list is empty.")
 
-@app.get("/causes{id}", status=200)
+@app.get("/causes{cause_id}", status_code=200)
 async def delete_cause_by_id(cause_id : id):
     if causes:
         for cause in causes:
-            if cause["id"] == id and cause["amount"] > 0.0:
+            if cause["cause_id"] == cause_id and cause["amount"] > 0.0:
                 return "The amount of this cause is $ " + cause["amount"]
                 cause_deleted = causes.pop(cause)
                 return {
@@ -56,3 +69,41 @@ async def delete_cause_by_id(cause_id : id):
                  }
             raise HTTPException(status_code=404, detail="Cause not found.")
     raise HTTPException(status_code=400, detail="The list is empty.")
+
+
+#Cria um hash para as transações
+def create_transaction_hash():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=64))
+
+#Converte a doação realizada na criptomoeda da Ethereum em dólar americano
+def convert_ether_in_dollar(value : float):
+    return value * 3664.03
+
+@app.post("/donations", status_code=200)
+async def create_donation(donation : Donation):
+
+    cause = next((sake for sake in causes if sake.cause_id == donation.cause_id), None)
+    
+    if cause is None:
+        raise HTTPException(status_code=400, detail="Cause does not exist.")
+
+    transaction_hash = create_transaction_hash()
+
+    donation_dollar = convert_ether_in_dollar(donation.value)
+
+    cause.amount += donation_dollar
+
+    donations.append({
+        "cause_id" : cause.cause_id,
+        "donation_id" : transaction_hash,
+        "donation" : donation_dollar
+    })
+
+    return {
+        "status" : "Success",
+        "message" : "Donation processed successfully!",
+        "data" : {
+            "donation" :donation,
+            "transaction_hash" : transaction_hash 
+        }
+    }
