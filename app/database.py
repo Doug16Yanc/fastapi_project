1fc00d87d1af4d73c2754c4f0ab513b0de4428dd
douglas <douglasholanda3195@gmail.com>
2024-12-07 16:09:57 -0500

    Máscara de credencial

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-POSTGRESQL_DATABASE_URL = "postgresql://postgres:7856@127.0.0.1:5432/fastapi"
+POSTGRESQL_DATABASE_URL = "postgresql://postgres:1111@127.0.0.1:5432/fastapi"
 
 engine = create_engine(
     POSTGRESQL_DATABASE_URL, echo=True
